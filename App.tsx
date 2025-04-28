a1450fe80b9c869933e6cbad0a6c3d51fad64a18
MakandarSahil <sahilmakandar15@gmail.com>
2025-04-28 20:39:56 +0530

    notification updates - still need to show number only dot is showing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
-import NotificationProvider from './src/context/NotificationContext';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import DocumentsProvider from './src/context/DocumentsContext';
+import notifee from '@notifee/react-native';
+import NotificationProvider from './src/context/NotificationContext';
+
 
 const App = () => {
+  // useEffect(() => {
+  //   const setupBadge = async () => {
+  //     try {
+  //       await notifee.requestPermission();
+  //       await notifee.setBadgeCount(5);
+  //     } catch (error) {
+  //       console.error('Error setting badge:', error);
+  //     }
+  //   };
+
+  //   setupBadge();
+  // }, []);
   return (
     <SafeAreaProvider>
       <NotificationProvider>
